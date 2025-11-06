fbad10c6518472a8539a56080799fba72d4cc84b
rizkyXicarus <rijalazhar07@gmail.com>
2025-11-06 17:52:24 +0700

    ayam laos

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,21 @@ use Illuminate\Support\Facades\Route;
     return view('expense');
 });*/
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('incomes');
+});*/
+
+/*Route::get('/', function () {
+    return view('account');
+});*/
+
+Route::get('/', function () {
+    return redirect('/cashier');
 });
+
+// Halaman kasir utama sekarang memuat file 'cashier_food'
+Route::get('/cashier', function () {
+    return view('cashier_food');
+})->name('cashier'); // Kita beri nama 'cashier'
 
 // ... (Rute login, dll) ...
