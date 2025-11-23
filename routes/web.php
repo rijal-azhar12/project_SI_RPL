6f3e2aff413ab2704b9e5e235cf64b6cf58582df
Notum <qholdirheody@gmail.com>
2025-11-23 20:49:05 +0700

    Route Fix

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use App\Http\Controllers\IncomeController;
 |--------------------------------------------------------------------------
 */
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
+
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
