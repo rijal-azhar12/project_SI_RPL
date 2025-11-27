a320d3865daec4f073702591b72905bea45473f6
Notum <qholdirheody@gmail.com>
2025-11-27 20:13:01 +0700

    Remove Comment

--- database/migrations/2023_11_13_000000_create_menu_table.php
+++ database/migrations/2023_11_13_000000_create_menu_table.php
@@ -11,18 +11,17 @@ class CreateMenuTable extends Migration
      *
      * @return void
      */
+
     public function up()
     {
         Schema::create('menu', function (Blueprint $table) {
-            $table->id('id_menu'); // Auto-incrementing primary key
-            $table->text('gambar_menu'); // Changed to text for URL
-            $table->string('nama_menu', 255); // Max length 255
-            $table->integer('stok_menu')->default(0); // Default to 0
-            $table->text('deskripsi_menu')->nullable(); // Made nullable
-            $table->enum('kategori_menu', ['Drink', 'Food']); // Adjusted enum values
-            $table->integer('harga_menu'); // Prices
-
-            $table->timestamps(); // Adds created_at and updated_at columns
+            $table->id('id_menu');
+            $table->text('gambar_menu');
+            $table->string('nama_menu', 255);
+            $table->integer('stok_menu')->default(0);
+            $table->text('deskripsi_menu')->nullable();
+            $table->enum('kategori_menu', ['Makanan', 'Minuman']);
+            $table->integer('harga_menu');
         });
     }
 
