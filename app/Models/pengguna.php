34185d9053b954be7ad6d9216bd56cb38af7edec
Notum <qholdirheody@gmail.com>
2025-11-27 15:46:00 +0700

    Remove Comment

--- app/Models/pengguna.php
+++ app/Models/pengguna.php
@@ -5,14 +5,11 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-// Nama class sekarang 'Pengguna'
 class Pengguna extends Authenticatable
 {
     use Notifiable;
 
-    protected $table = 'user'; // Tetap mengarah ke tabel 'user'
+    protected $table = 'user';
     protected $primaryKey = 'id_user';
     public $timestamps = false;
-    
-    // ... (isi $fillable dan relasi lainnya) ...
 }
